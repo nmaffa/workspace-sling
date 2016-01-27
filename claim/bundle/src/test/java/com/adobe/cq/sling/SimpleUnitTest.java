fc06d6697b6ecfa8a7da313536b55355788e0c4e
Nick Maffattone <nmaffattone@sapient.com>
2016-01-27 14:42:32 -0500

    First edit - log not working

--- claim/bundle/src/test/java/com/adobe/cq/sling/SimpleUnitTest.java
+++ claim/bundle/src/test/java/com/adobe/cq/sling/SimpleUnitTest.java
@@ -4,11 +4,21 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.adobe.cq.service.ProspectService;
+import com.adobe.cq.service.ProspectServiceImpl;
+
 public class SimpleUnitTest {
 
     @Test
     public void someTest() {
         assertTrue(true);
+    }
+    
+    @Test
+    public void prospectServiceFail(){
+    	ProspectService prospectService = new ProspectServiceImpl();
+    	
+    	prospectService.approveProspects("");
     }
 
 }
